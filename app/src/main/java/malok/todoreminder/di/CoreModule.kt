684d1bd04838cec2c7c131076111ff3ae0914239
Malik <bittiev.malik@mail.ru>
2026-03-26 13:47:17 +0300

    refactor: improve Room database initialization (add @Volatile + synchronized)

--- app/src/main/java/malok/todoreminder/di/CoreModule.kt
+++ app/src/main/java/malok/todoreminder/di/CoreModule.kt
@@ -1,6 +1,5 @@
 package malok.todoreminder.di
 
-import androidx.room.Room
 import malok.testtask.core_data.db.AppDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -9,12 +8,8 @@ import malok.testtask.core_data.repository.TaskRepositoryImpl
 
 val coreModule = module {
 
-    single {
-        Room.databaseBuilder(
-            androidContext(),
-            AppDatabase::class.java,
-            "app_db"
-        ).build()
+    single<AppDatabase> {
+        AppDatabase.getDatabase(androidContext())
     }
 
     single { get<AppDatabase>().taskDao() }
--- core_data/src/main/java/malok/testtask/core_data/db/AppDatabase.kt
+++ core_data/src/main/java/malok/testtask/core_data/db/AppDatabase.kt
@@ -2,11 +2,34 @@ package malok.testtask.core_data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.Room
 
 @Database(
     entities = [TaskEntity::class],
-    version = 3
+    version = 3,
+    exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_db"
+                )
+                    .fallbackToDestructiveMigration(true)  //drop tables
+                    // .addMigrations(MIGRATION_2_3, MIGRATION_3_4) // для миграции пока не надо
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
 }
